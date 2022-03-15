4247300b98d68951db7c29d8b64fdf8b16d67d00
pjgorski <piotr@kod.com.pl>
2022-03-15 02:38:50 +0100

    file analysis

--- scripts/analyze_fig1.jl
+++ scripts/analyze_fig1.jl
@@ -1,3 +1,40 @@
 using DrWatson
 quickactivate(@__DIR__)
 
+using StatsBase
+using Plots
+using DataFrames
+using CSV
+
+# get data 
+res = DrWatson.collect_results(
+        datadir("sims"),
+        rinclude=[r"NumerFig1[.]*"]
+        #   black_list = bl,
+    )
+first(res, 10)
+
+# plotting
+N = 9
+attr_name = "BinaryAttributes" # ["BinaryAttributes", "OrderedAttributes", "UnorderedAttributes", "UnorderedPositiveAttributes"]
+gamma = unique(res.gamma)
+attr_degeneracy = 2 #It should be 2 for BA, 1000 for CA (OrderedAttributes). Otherwise 4. 
+
+params = @strdict N attr_name gamma attr_degeneracy
+dicts = dict_list(params)
+
+p = plot()
+for dict in dicts
+    inds = ones(Bool, size(res)[1])
+
+    for param in dict
+        inds .*= res[!, string(param[1])] .== param[2]
+    end
+
+    plot!(p, res.G[inds], res.LP[inds], lab = "gamma="*string(dict["gamma"]))
+end
+
+plot(p, xlabel = "G", ylabel = "P_{LP}", legend = :bottomright)
+attr = attr_name
+v = attr_degeneracy
+title!(savename(@ntuple(N, attr, v)))
