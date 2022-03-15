102eb43c777f217cbc5a251fd244d3f4557dfae0
pjgorski <piotr@kod.com.pl>
2022-03-15 14:55:31 +0100

    renaming files

--- scripts/using_current_version.jl
+++ scripts/using_current_version.jl
@@ -8,12 +8,4 @@ using DifferentialEquations
 g = 5
 attr = BinaryAttributes(g)
 
-calc_heider_attr(
-    45,
-    0.0,
-    1000.0,
-    Heider7!,
-    AutoTsit5(Rodas5(autodiff = false)),
-    true,
-    attr,
-)
+calc_heider_attr(45, 0.0, 1000.0, Heider7!, AutoTsit5(Rodas5(autodiff = false)), true, attr)
