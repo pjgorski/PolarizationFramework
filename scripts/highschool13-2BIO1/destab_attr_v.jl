f90f16a9e819d336804da085684f8c3f8d5cc184
pjgorski <piotr@kod.com.pl>
2022-11-02 13:46:39 +0100

    destab new sim files

--- scripts/highschool13-2BIO1/destab_attr_v.jl
+++ scripts/highschool13-2BIO1/destab_attr_v.jl
@@ -1,3 +1,4 @@
+# Highschool13 2BIO1 topology
 # Destabilizing polarization as a function of number of attributes. 
 # Figure 2
 using DrWatson
@@ -6,37 +7,46 @@ quickactivate(@__DIR__)
 using PolarizationFramework
 using Graphs
 
-gr = KarateGraph()
-
-specified_division = [
-    [3, 14, 20, 2, 1, 4, 8, 18, 22, 12, 13, 5, 11, 6, 7, 17],
-    [21, 30, 19, 24, 16, 27, 15, 23, 33, 34, 28, 29, 32, 26, 25, 31, 10, 9],
-]
-
+file = datadir("highschool13", "Highschool13-class2BIO1.lg")
+gr = loadgraph(file)
 ns = [nv(gr)]
-# ns = [5, 9]#[9,15,25]
+A = adjacency_matrix(gr)
+all_triads = get_triads(A)
+all_links = get_links_in_triads(all_triads)
+A = get_adj_necessary_links(size(A)[1], all_links; typ = Float64);
+
+all_triads = get_triads(A);
+
+#Heider72!
+all_links = get_links_in_triads(all_triads)
+triads_around_links_dict = get_triangles_around_links(all_triads)
+counts = link_triangles_count(triads_around_links_dict; links = all_links)
+triads_count_mat = PolarizationFramework.link_triangles_mat_inv(ns[1], all_links, counts)
+
 reps = [100]
 reps_dict = Dict(zip(ns, reps))
 gs = [1, 3, 5, 7, 11]
 # gs = [5]
 threshold = 0.5;
-vs = [4, 8, 16, 32, 64, 128, 256, 512, 1000];
+vs = [2, 4, 8, 16, 32, 64, 128, 256, 512, 1000];
 
 attr_types = ["UA", "OA", "UPA"]
 
 gammas = [0.5, 1.5, 6]
 
-all_params = @strdict(ns, gs, threshold, vs, attr_types)
+larger_sizes = [@onlyif(i <= "ns" < 2 * i, i) for i = 1:maximum(ns)]
+
+all_params = @strdict(ns, gs, threshold, vs, attr_types, larger_sizes)
 dicts = dict_list(all_params)
 [d["reps"] = reps_dict[d["ns"]] for d in dicts] #adding reps
 # [d["gammas"] = gammas for d in dicts] #adding gammas
 
 for params in dicts
-    n, g, attr_type, v, rep = let
-        @unpack ns, threshold, reps, gs, attr_types, vs = params
-        ns, gs, attr_types, vs, reps
+    n, g, attr_type, v, rep, larger_size = let
+        @unpack ns, threshold, reps, gs, attr_types, vs, larger_sizes = params
+        ns, gs, attr_types, vs, reps, larger_sizes
     end
-    println("Started ", @ntuple(g, attr_type, v))
+    println("Started H13", @ntuple(n, g, attr_type, v, larger_size))
 
     if attr_type == "UA"
         attr = UnorderedAttributes(g, threshold, v)
@@ -55,16 +65,17 @@ for params in dicts
         n,
         attr,
         gammas,
-        -1,
+        larger_size,
         rep,
         3000.0,
-        "Heider9!",
+        "Heider722!",
         disp_each = 0,
         disp_more_every = 600,
         save_each = 600,
-        files_folder = ["data", "karate-sims"],
-        filename_prefix = "DesKarv",
-        graph = gr,
-        specified_division = specified_division,
+        files_folder = ["data", "highschool13-sims"],
+        filename_prefix = "DesKarG",
+        all_links_mat = A,
+        all_triads = all_triads,
+        triads_count_mat = triads_count_mat,
     )
 end
